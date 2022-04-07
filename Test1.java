5d03bba93d01914400d7905d3e6fa0adecc6b15a
luren123 <90602982+li382112772@users.noreply.github.com>
2022-04-07 02:54:38 +0000

    0407commit

---
+++ Test1.java
@@ -0,0 +1,20 @@
+package com.niu;
+
+import com.niu.service.MenuService;
+
+import java.util.Scanner;
+
+/**
+ * @description: 主类
+ * @author: nxq email: niuxiangqian163@163.com
+ * @createDate: 2020/12/24 6:54 下午
+ * @updateUser: nxq email: niuxiangqian163@163.com
+ * @updateDate: 2020/12/24 6:54 下午
+ * @updateRemark:
+ * @version: 1.0
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+       System.out.print("123");
+    }
+}
